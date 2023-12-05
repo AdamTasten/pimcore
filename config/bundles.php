1803d047ffaa26612a225305d86b98e7c6d55f44
Adam Zdeschits <adam.zdeschits@innowise-group.com>
2023-12-05 09:45:36 +0100

    add datahub bundle

--- config/bundles.php
+++ config/bundles.php
@@ -1,5 +1,8 @@
 <?php
 
+use Pimcore\Bundle\DataHubBundle\PimcoreDataHubBundle;
+
 return [
     //Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    PimcoreDataHubBundle::class => ['all' => true],
 ];
--- var/classes/definition_Car.php
+++ var/classes/definition_Car.php
@@ -6,6 +6,8 @@
  *
  * Fields Summary:
  * - Name [input]
+ * - ProductNumber [input]
+ * - Price [numeric]
  */
 
 return Pimcore\Model\DataObject\ClassDefinition::__set_state(array(
@@ -15,7 +17,7 @@ return Pimcore\Model\DataObject\ClassDefinition::__set_state(array(
    'title' => '',
    'description' => '',
    'creationDate' => NULL,
-   'modificationDate' => 1701178673,
+   'modificationDate' => 1701445363,
    'userOwner' => 2,
    'userModification' => 2,
    'parentClass' => '',
@@ -85,6 +87,66 @@ return Pimcore\Model\DataObject\ClassDefinition::__set_state(array(
             ),
              'unique' => false,
              'showCharCount' => false,
+             'width' => '',
+             'defaultValueGenerator' => '',
+          )),
+          1 => 
+          Pimcore\Model\DataObject\ClassDefinition\Data\Input::__set_state(array(
+             'name' => 'ProductNumber',
+             'title' => 'Product Number',
+             'tooltip' => '',
+             'mandatory' => false,
+             'noteditable' => false,
+             'index' => false,
+             'locked' => false,
+             'style' => '',
+             'permissions' => NULL,
+             'fieldtype' => '',
+             'relationType' => false,
+             'invisible' => false,
+             'visibleGridView' => false,
+             'visibleSearch' => false,
+             'blockedVarsForExport' => 
+            array (
+            ),
+             'defaultValue' => NULL,
+             'columnLength' => 190,
+             'regex' => '',
+             'regexFlags' => 
+            array (
+            ),
+             'unique' => false,
+             'showCharCount' => false,
+             'width' => '',
+             'defaultValueGenerator' => '',
+          )),
+          2 => 
+          Pimcore\Model\DataObject\ClassDefinition\Data\Numeric::__set_state(array(
+             'name' => 'Price',
+             'title' => 'Price',
+             'tooltip' => '',
+             'mandatory' => false,
+             'noteditable' => false,
+             'index' => false,
+             'locked' => false,
+             'style' => '',
+             'permissions' => NULL,
+             'fieldtype' => '',
+             'relationType' => false,
+             'invisible' => false,
+             'visibleGridView' => false,
+             'visibleSearch' => false,
+             'blockedVarsForExport' => 
+            array (
+            ),
+             'defaultValue' => NULL,
+             'integer' => false,
+             'unsigned' => true,
+             'minValue' => 0.0,
+             'maxValue' => NULL,
+             'unique' => false,
+             'decimalSize' => 10,
+             'decimalPrecision' => 2,
              'width' => '',
              'defaultValueGenerator' => '',
           )),
